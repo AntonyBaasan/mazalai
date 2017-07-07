ced33007454fdb917e4066343190bf3e461bbbbd
Ankhbayar <antony.baasan@gmail.com>
2017-07-06 23:43:04 -0400

    create LogAn projects

---
+++ LogAn/LogAnalyzer.cs
@@ -0,0 +1,12 @@
+public class LogAnalyzer
+{
+    public bool IsValidLogFileName(string fileName)
+    {
+        if (fileName.EndsWith(".SLF"))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
